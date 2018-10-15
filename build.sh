b32b9611ed8f58d5a3b8262d2c6c4c2a795e8144
gitlab-ci <gitlab-ci@test.com>
2018-10-15 10:41:36 +0000

    Updated content for dev -

--- build.sh
+++ build.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-if [ "{$1}" == "dev" ]; then
+if [ "$1" == "dev" ]; then
         cd isentia-web
 	hugo new posts/${CI_JOB_ID}.md
         fortune >> posts/${CI_JOB_ID}.md
         hugo -D
         cd -
+        git config --global user.email "gitlab-ci@test.com"
+ 	git config --global user.name "gitlab-ci"
         git commit -am "Updated content for dev - ${CI_JOB_ID}"
         git push -u origin master
 
-elif [ "{$1}" == "staging" ]; then
+elif [ "$1" == "staging" ]; then
 	cd isentia-web
 	hugo -D
         cd - 
+        git config --global user.email "gitlab-ci@test.com"
+ 	git config --global user.name "gitlab-ci"
         git tag "v${CI_JOB_ID}"
         git commit -am "Updated content for staging - ${CI_JOB_ID}"
         git push -u origin master 
